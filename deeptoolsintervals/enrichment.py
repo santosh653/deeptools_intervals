4e557a3a9947c4e7daaf022fd57dc8b92bf1e7e5
dpryan79 <dpryan79@gmail.com>
2016-07-25 14:55:38 +0200

    Should have tested before committing

--- deeptoolsintervals/enrichment.py
+++ deeptoolsintervals/enrichment.py
@@ -111,7 +111,7 @@ class Enrichment(GTF):
 
         feature = cols[2]
         if "deepTools_group" in cols[8]:
-            s = csv.reader([cols[8]], delimiter=' ')
+            s = csv.reader([cols[8]], delimiter=' ').next()
             if s[-1] != "deepTools_group":
                 feature = s[s.index("deepTools_group") + 1].rstrip(";")
 
@@ -136,7 +136,7 @@ class Enrichment(GTF):
 
                 feature = cols[2]
                 if "deepTools_group" in cols[8]:
-                    s = shlex.split(cols[8])
+                    s = csv.reader([cols[8]], delimiter=" ").next()
                     if s[-1] != "deepTools_group":
                         feature = s[s.index("deepTools_group") + 1].rstrip(";")
 
